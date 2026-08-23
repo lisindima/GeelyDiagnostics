d541044517e60ba193f678612c79dcb38f8ce93a
Lisin Dmitriy <43087859+lisindima@users.noreply.github.com>
2026-08-23 21:22:42 +0300

    Release 0.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.geelydiagnostics.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "0.12.2"
+        versionCode = 27
+        versionName = "0.13.0"
     }
 
     buildTypes {
